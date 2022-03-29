e3b1cdf0b102e35f9f067705141e76cc55643130
Suso <41950530+Suso1@users.noreply.github.com>
2022-03-29 13:39:22 +0200

    GMC Skeleton

--- src/gmc.c
+++ src/gmc.c
@@ -1,5 +1,45 @@
 #include "gmc.h"
 
 gmc_result gmc(matrix * X, uint m, uint c, double lambda, bool normalize) {
+    uint num = X[0].w;
 
+    //Normalize data
+    if(normalize) {
+        //TODO
+    }
+
+    //Initialize SIG matrices
+    matrix * S0 = malloc(m * sizeof(matrix *));
+    for(int i = 0; i < m; i++) S0[i] = initSIG(X[i], PN);
+
+    matrix U = newMatrix(num, num);
+    //TODO: U starts as average of SIG matrices
+    //TODO: Divide each row of U by its own sum
+
+    //TODO: The thing with symmetric U into eigenvectors, gets F
+
+    double wI = 1.0 / m;
+    matrix w = newMatrix(m, 1);
+    for(int i = 0; i < m; i++) w.data[i] = wI;
+
+    matrix * ed = malloc(m * sizeof(matrix *));
+    matrix * idxx = malloc(m * sizeof(matrix *));
+    for(int i = 0; i < m; i++) {
+        ed[i] = sqrDist(X[i]);
+        //TODO: Store sort into idxx
+    }
+
+    for(int it = 0; i < NITER; i++) {
+        //TODO: Update S0
+
+        //TODO: Update w
+
+        //TODO: Update U
+
+        //TODO: The thing with symmetric U into eigenvectors, gets F
+
+        //TODO: Lambda stuff
+    }
+
+    //TODO: Final clustering
 }
--- src/gmc.h
+++ src/gmc.h
@@ -13,4 +13,9 @@ typedef struct gmc_result {
 
 gmc_result gmc(matrix * X, uint m, uint c, double lambda, bool normalize);
 
+#define NITER 20
+#define ZR 10e-11
+#define PN 15
+#define IS_LOCAL 1
+
 #endif
